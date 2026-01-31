8ce9b511832fed2284e7113cf0110e2afb0bdb85
Enrique PN <132113851+EnriqueMK@users.noreply.github.com>
2026-01-31 03:44:09 -0300

    Adicionando os arquivos na view do banco de dados

--- src/resources/sql/ddl/views/vw_limite_cartao.sql
+++ src/resources/sql/ddl/views/vw_limite_cartao.sql
@@ -1,9 +1,12 @@
+use sql_database_project;
+
 -- Consulta limites dos clientes em ordem decrescente
-SELECT cartao.cartao_id AS "Código do Cartão",
-    cartao.limite AS "Limite",
-    cliente.nome AS "Nome",
-    cliente.cpf AS "CPF",
-    cliente.telefone AS "Telefone"
-    FROM cartao
-    INNER JOIN cliente ON cartao.cliente_id = cliente.cliente_id
-    ORDER BY cartao.limite DESC;
+CREATE VIEW limite_cartao AS
+    SELECT cartao.cartao_id AS "Código do Cartão",
+        cartao.limite AS "Limite",
+        cliente.nome AS "Nome",
+        cliente.cpf AS "CPF",
+        cliente.telefone AS "Telefone"
+        FROM cartao
+        INNER JOIN cliente ON cartao.cliente_id = cliente.cliente_id
+        ORDER BY cartao.limite DESC;
--- src/resources/sql/ddl/views/vw_vencimento_cartao.sql
+++ src/resources/sql/ddl/views/vw_vencimento_cartao.sql
@@ -1,9 +1,10 @@
 -- Consulta qual o cartão mais próximo esta para vencer
-SELECT cartao.cartao_id AS "Código do Cartão",
-    cartao.vencimento AS "Vencimento",
-    cliente.nome AS "Nome",
-    cliente.cpf AS "CPF",
-    cliente.telefone AS "Telefone"
-    FROM cartao
-    INNER JOIN cliente ON cartao.cliente_id = cliente.cliente_id
-    ORDER BY cartao.vencimento; 
+CREATE VIEW vencimento_cartao AS
+    SELECT cartao.cartao_id AS "Código do Cartão",
+        cartao.vencimento AS "Vencimento",
+        cliente.nome AS "Nome",
+        cliente.cpf AS "CPF",
+        cliente.telefone AS "Telefone"
+        FROM cartao
+        INNER JOIN cliente ON cartao.cliente_id = cliente.cliente_id
+        ORDER BY cartao.vencimento; 
